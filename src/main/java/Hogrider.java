f96099038c81ff8419bed557cdaafc8c41adf1ef
zenweilow <lowzenwei@gmail.com>
2026-01-30 15:45:45 +0800

    Level-3: Marking and Unmarking Tasks

--- src/main/java/Hogrider.java
+++ src/main/java/Hogrider.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Hogrider {
 
     private static final int MAX_ITEMS = 100;
-    private final ArrayList<String> items = new ArrayList<>();
+    private final ArrayList<Task> items = new ArrayList<>();
 
     public static void main(String[] args) {
         new Hogrider().run();
@@ -22,9 +22,12 @@ public class Hogrider {
                 showExit();
                 break;
             }
-
-            if (input.equals("list")) {
+            else if (input.equals("list")) {
                 showList();
+            } else if (input.startsWith("mark ")) {
+                markTask(input);
+            } else if (input.startsWith("unmark ")) {
+                unmarkTask(input);
             } else {
                 addItem(input);
             }
@@ -39,7 +42,7 @@ public class Hogrider {
         if (items.size() >= MAX_ITEMS) {
             System.out.println(" eh bro cannot add already, max 100 items ");
         } else {
-            items.add(input);
+            items.add(new Task(input));
             System.out.println(" added: " + input);
         }
 
@@ -60,12 +63,37 @@ public class Hogrider {
             System.out.println(" nothing here leh");
         } else {
             for (int i = 0; i < items.size(); i++) {
-                System.out.println(" " + (i + 1) + ". " + items.get(i));
+                System.out.println(" " + (i + 1) + "." + items.get(i));
             }
         }
 
         System.out.println("____________________________________________________________");
     }
+
+    private void markTask(String input) {
+        int index = Integer.parseInt(input.substring(5)) - 1;
+
+        Task task = items.get(index);
+        task.mark();
+
+        System.out.println("____________________________________________________________");
+        System.out.println(" Nice! I've marked this task as done:");
+        System.out.println("   " + task);
+        System.out.println("____________________________________________________________");
+    }
+
+    private void unmarkTask(String input) {
+        int index = Integer.parseInt(input.substring(7)) - 1;
+
+        Task task = items.get(index);
+        task.unmark();
+
+        System.out.println("____________________________________________________________");
+        System.out.println(" OK, I've marked this task as not done yet:");
+        System.out.println("   " + task);
+        System.out.println("____________________________________________________________");
+    }
+
 
     private void showExit() {
         System.out.println("____________________________________________________________");
---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    private final String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void mark() {
+        isDone = true;
+    }
+
+    public void unmark() {
+        isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return isDone ? "X" : " ";
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+}
